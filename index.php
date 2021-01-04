74c97cbc0256ab838ce927417bf871d5620c6c6c
K1ngp1nDev <s.kulitskiy@gmail.com>
2021-01-04 22:03:47 +0200

    add package

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
-require_once 'vendor/autoload.php';
-use GuzzleHttp\Client;
 
-$client = new GuzzleHttp\Client();
-$response = $client->request('GET', 'https://itea.ua/');
-echo $response->getBody();
+use src\Multiple;
+require 'vendor/autoload.php';
+//use GuzzleHttp\Client;
+//
+//$client = new GuzzleHttp\Client();
+//$response = $client->request('GET', 'https://itea.ua/');
+//echo $response->getBody();
+//use math\src\Multiple;
+$tmp = new Multiple();
+echo "Sum: " . $tmp->sum(10,20) . "\n";
