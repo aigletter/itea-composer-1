bce2c7782ee1586d1da487c7844b1b89fbb61050
K1ngp1nDev <s.kulitskiy@gmail.com>
2021-01-05 11:05:39 +0200

    include custom library math from packagist

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <?php
 
 use src\Multiple;
+
 require 'vendor/autoload.php';
+require 'vendor/k1ngp1n/math/Multiple.php';
 //use GuzzleHttp\Client;
 //
 //$client = new GuzzleHttp\Client();
 //$response = $client->request('GET', 'https://itea.ua/');
 //echo $response->getBody();
 //use math\src\Multiple;
+
 $tmp = new Multiple();
 echo "Sum: " . $tmp->sum(10,20) . "\n";
